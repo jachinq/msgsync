e35d633942fd68d92c8b112da975352553dba53c
jachin <jachinq@163.com>
2025-01-20 18:37:09 +0800

    feat: adapt path

--- src/gotify.js
+++ src/gotify.js
@@ -23,20 +23,29 @@ class GotifyConfig {
 
   async sendMsg({ date, content }) {
     try {
-      const url = this.getUrl();
+      let url = this.getUrl();
       const token = this.getToken();
       const data = {
         message: content,
         title: 'MsgSync:' + date,
       };
-      const response = await fetch(url + '/message?token=' + token, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-      return response.ok;
+      if (url && token) {
+        if (url.endsWith('/')) {
+          url = url.slice(0, -1);
+        }
+        if (url.endsWith('/message')) {
+          url = url.slice(0, -8);
+        }
+        // console.log(url + '/message?token=' + token);
+        const response = await fetch(url + '/message?token=' + token, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+        return response.ok;
+      }
     } catch (error) {
       console.error(error);
       return false;
